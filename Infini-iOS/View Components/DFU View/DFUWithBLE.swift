2742872f84c8a6b17869850e0c318c38c83011e8
xan-m <xanm@emry.me>
2021-09-15 15:04:55 -0700

    Missed a file in last commit...

---
+++ Infini-iOS/View Components/DFU View/DFUWithBLE.swift
@@ -0,0 +1,96 @@
+//
+//  DFUWithBLE.swift
+//  DFUWithBLE
+//
+//  Created by Alex Emry on 9/15/21.
+//  
+//
+    
+
+import Foundation
+import SwiftUI
+
+struct DFUWithBLE: View {
+	@Environment(\.colorScheme) var colorScheme
+	@EnvironmentObject var bleManager: BLEManager
+	@EnvironmentObject var dfuUpdater: DFU_Updater
+	
+	@State var openFile = false
+	@State private var firmwareFilename = ""
+	@State private var firmwareSelected: Bool = false
+	@State private var firmwareURL: URL = URL(fileURLWithPath: "")
+	@State private var updateStarted: Bool = false
+	
+	var body: some View {
+		ZStack {
+			VStack (alignment: .leading){
+				Text("Firmware Update")
+					.font(.largeTitle)
+					.padding()
+				
+				HStack {
+					Text("Current Firmware: ")
+						.font(.title)
+					Text(bleManager.firmwareVersion)
+						.font(.title)
+				}.padding()
+				
+				
+				if firmwareSelected {
+					Text("Selected Firmware File: ")
+						.font(.title)
+						.padding(.horizontal)
+					Text(firmwareFilename)
+						.padding(.horizontal)
+				}
+				Spacer()
+				
+				if updateStarted {
+					DFUProgressBar().environmentObject(dfuUpdater)
+						.frame(height: 40 ,alignment: .center)
+						.padding()
+				}
+				
+				DFUFileSelectButton(openFile: $openFile)
+					.environmentObject(bleManager)
+					.fileImporter(isPresented: $openFile, allowedContentTypes: [.zip]) {(res) in
+						// this fileImporter allows user to select the zip from local storage. DFU updater just wants the local URL to the file, so we're opening privileged access, grabbing the url, and closing privileged access
+						do{
+							let fileUrl = try res.get()
+							
+							guard fileUrl.startAccessingSecurityScopedResource() else { return }
+							
+							self.firmwareSelected = true
+							self.firmwareFilename = fileUrl.lastPathComponent
+							self.firmwareURL = fileUrl.absoluteURL
+							
+							fileUrl.stopAccessingSecurityScopedResource()
+						} catch{
+							print ("error reading")
+							print (error.localizedDescription)
+						}
+					}
+				
+				DFUStartTransferButton(updateStarted: $updateStarted, firmwareSelected: $firmwareSelected, firmwareURL: $firmwareURL)
+					.environmentObject(dfuUpdater)
+					.environmentObject(bleManager)
+				
+				VStack{
+					if dfuUpdater.transferCompleted {
+						DFUComplete()
+							.cornerRadius(10)
+							.onAppear() {
+								DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+									dfuUpdater.transferCompleted = false
+								})
+								updateStarted = false
+								firmwareURL = URL(fileURLWithPath: "")
+								firmwareSelected = false
+								firmwareFilename = ""
+							}
+					}
+				}.transition(.opacity).animation(.easeInOut(duration: 1.0))
+			}
+		}
+	}
+}
