9307e1e2c1f0e18c2b6b48e2d530305f0501afa4
xan-m <xanm@emry.me>
2021-12-02 14:03:16 -0800

    Removed large button from send notification sheet and replaced it with a simple "send" button at the top that matches the "close" button. This should help people with smaller screens and/or accessibility tweaks

--- InfiniLink/View Components/Sheets/ArbitraryNotificationView.swift
+++ InfiniLink/View Components/Sheets/ArbitraryNotificationView.swift
@@ -18,7 +18,26 @@ struct ArbitraryNotificationSheet: View {
 	var placeholderString = "enter text here"
 	
 	var body: some View {
-		SheetCloseButton()
+		ZStack {
+			SheetCloseButton()
+			HStack {
+				Spacer()
+				Button {
+					UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+					if !notificationBody.isEmpty || !notificationTitle.isEmpty {
+						BLEWriteManager.init().sendNotification(title: notificationTitle, body: notificationBody)
+					}
+					SheetManager.shared.showSheet = false
+					SheetManager.shared.sheetSelection = .connect
+				} label: {
+					Text("Send")
+						.font(.title2)
+				}
+				.padding()
+				
+			}
+		}
+
 		VStack{
 			Text(NSLocalizedString("send_notification", comment: ""))
 				.font(.title)
@@ -34,6 +53,7 @@ struct ArbitraryNotificationSheet: View {
 				.padding(.horizontal)
 			TextEditor(text: $notificationBody)
 				.padding(.horizontal)
+<<<<<<< HEAD
 			Button(action: {
 				UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
 				if !notificationBody.isEmpty || !notificationTitle.isEmpty {
@@ -52,6 +72,8 @@ struct ArbitraryNotificationSheet: View {
 					.padding(.horizontal, 20)
 					.padding(.bottom)
 			}
+=======
+>>>>>>> 7869ee0 (Removed large button from send notification sheet and replaced it with a simple "send" button at the top that matches the "close" button. This should help people with smaller screens and/or accessibility tweaks)
 		}
 	}
 }
