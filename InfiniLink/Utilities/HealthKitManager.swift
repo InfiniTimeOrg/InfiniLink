3db266e7d66db41bb9c1f5ea6a0c67fd0ec48df5
liamcharger <chargerelectronics19@gmail.com>
2024-01-04 12:15:44 -0500

    Fix crash with healthKit

--- InfiniLink/Utilities/HealthKitManager.swift
+++ InfiniLink/Utilities/HealthKitManager.swift
@@ -9,8 +9,6 @@ import Foundation
 import HealthKit
 
 class HealthKitManager: ObservableObject {
-    @Published var stepCount: Int = 0
-    @Published var heartRate: Int = 0
     var healthStore: HKHealthStore?
     
     init() {
@@ -24,48 +22,48 @@ class HealthKitManager: ObservableObject {
         
         let stepsSample = HKQuantitySample(type: stepType, quantity: HKQuantity.init(unit: HKUnit.count(), doubleValue: stepsToAdd), start: date, end: date)
         
-        if let healthStore = healthStore {
-            healthStore.save(stepsSample, withCompletion: { success, error in
-                
-                if error != nil {
-                    print(error?.localizedDescription as Any)
-                    return
-                }
-                
-                if success {
-                    print("Steps successfully saved in HealthKit")
-                    return
-                } else {
-                    print("Unhandled case!")
-                }
-                
-            })
+        if healthStore?.authorizationStatus(for: stepType) == .sharingAuthorized {
+            if let healthStore = healthStore {
+                healthStore.save(stepsSample, withCompletion: { success, error in
+                    
+                    if error != nil {
+                        print(error?.localizedDescription as Any)
+                        return
+                    }
+                    
+                    if success {
+                        print("Steps successfully saved in HealthKit")
+                        return
+                    } else {
+                        print("Unhandled case!")
+                    }
+                    
+                })
+            }
         }
     }
     
     func writeHeartRate(date: Date, dataToAdd: Double) {
         let heartRateType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
-        
-        let heartRateSample = HKQuantitySample(type: heartRateType, quantity: HKQuantity.init(unit: HKUnit.count(), doubleValue: dataToAdd), start: date, end: date)
-        
-        if let healthStore = healthStore {
-            healthStore.save(heartRateSample, withCompletion: { success, error in
-                
-                if error != nil {
-                    print(error?.localizedDescription as Any)
-                    return
-                }
-                
-                if success {
-                    print("Steps successfully saved in HealthKit")
-                    return
-                } else {
-                    print("Unhandled case!")
-                }
-                
-            })
+
+        let heartRateSample = HKQuantitySample(type: heartRateType, quantity: HKQuantity(unit: HKUnit.count().unitDivided(by: .minute()), doubleValue: dataToAdd), start: date, end: date)
+
+        if healthStore?.authorizationStatus(for: heartRateType) == .sharingAuthorized {
+            if let healthStore = healthStore {
+                healthStore.save(heartRateSample, withCompletion: { success, error in
+                    if let error = error {
+                        print(error.localizedDescription)
+                        return
+                    }
+
+                    if success {
+                        print("Heart rate successfully saved in HealthKit")
+                    } else {
+                        print("Unhandled case!")
+                    }
+                })
+            }
         }
-        
     }
     
     func requestAuthorization() {
@@ -79,67 +77,6 @@ class HealthKitManager: ObservableObject {
                 print(error)
             }
         }
-    }
-    
-    func fetchStepCount() {
-        let healthStore = HKHealthStore()
-        let stepType = HKSampleType.quantityType(forIdentifier: .stepCount)!
-        
-        let calendar = Calendar.current
-        let today = calendar.startOfDay(for: Date())
-        let predicate = HKQuery.predicateForSamples(withStart: today, end: Date(), options: .strictEndDate)
-        
-        var stepCount: Int = 0
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        let query = HKStatisticsQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
-            defer {
-                semaphore.signal()
-            }
-            
-            guard let result = result, let sum = result.sumQuantity() else {
-                return
-            }
-            
-            stepCount = Int(sum.doubleValue(for: .count()))
-        }
-        
-        healthStore.execute(query)
-        
-        semaphore.wait()
-        
-        self.stepCount = stepCount
-    }
-    
-    func fetchHeartRate() {
-        let healthStore = HKHealthStore()
-        let heartRateType = HKSampleType.quantityType(forIdentifier: .heartRate)!
-        
-        let calendar = Calendar.current
-        let today = calendar.startOfDay(for: Date())
-        let predicate = HKQuery.predicateForSamples(withStart: today, end: Date(), options: .strictEndDate)
-        
-        var heartRate: Int = 0
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        let query = HKSampleQuery(sampleType: heartRateType, predicate: predicate, limit: 1, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)]) { _, samples, error in
-            defer {
-                semaphore.signal()
-            }
-            
-            if let sample = samples?.first as? HKQuantitySample {
-                let heartRateQuantity = sample.quantity
-                let heartRateUnit = HKUnit(from: "count/min")
-                let heartRateBPM = Int(heartRateQuantity.doubleValue(for: heartRateUnit))
-                
-                heartRate = heartRateBPM
-            }
-        }
-        
-        healthStore.execute(query)
-        semaphore.wait()
-        
-        self.heartRate = heartRate
     }
 }
 
