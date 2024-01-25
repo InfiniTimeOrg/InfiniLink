f2241ad8d241f400981874df2c505bb25ffd565a
liamcharger <chargerelectronics19@gmail.com>
2024-01-25 16:17:24 -0500

    Minor UI improvements

--- InfiniLink/View Components/DFU View/DFUWithBLE.swift
+++ InfiniLink/View Components/DFU View/DFUWithBLE.swift
@@ -244,10 +244,9 @@ struct NoUpdate: View {
             VStack(alignment: .center , spacing: 6) {
                 Text("InfiniTime \(deviceInfo.firmware)")
                     .foregroundColor(.gray)
-                    .font(.title3.weight(.semibold))
+                    .font(.title2.weight(.semibold))
                 Text("InfiniTime " + NSLocalizedString("up_to_date", comment: ""))
                     .foregroundColor(.gray)
-                    .font(.system(size: 16))
             }
             .frame(maxWidth: .infinity, alignment: .bottom)
         }
