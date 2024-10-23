d132792d29b8d93be54cf8023cecb9b4417a0884
liamcharger <chargerelectronics19@gmail.com>
2024-10-23 18:26:11 -0400

    General: experiment with background tasks

--- InfiniLink/InfiniLinkApp.swift
+++ InfiniLink/InfiniLinkApp.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 import CoreData
+import BackgroundTasks
 
 @main
 struct InfiniLink: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     let persistenceController = PersistenceController.shared
     
     init() {
@@ -21,5 +24,48 @@ struct InfiniLink: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    let remindersManager = RemindersManager.shared
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.alexemry.Infini-iOS.remindercheck", using: nil) { task in
+            print("Setting up background task")
+            self.handleAppRefresh(task: task as! BGAppRefreshTask)
+        }
+        
+        scheduleAppRefresh()
+        
+        return true
+    }
+    
+    func scheduleAppRefresh() {
+        let request = BGAppRefreshTaskRequest(identifier: "com.alexemry.Infini-iOS.remindercheck")
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 30)
+        
+        DispatchQueue.main.async {
+            do {
+                try BGTaskScheduler.shared.submit(request)
+            } catch {
+                print("Could not schedule app refresh: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func handleAppRefresh(task: BGAppRefreshTask) {
+        let expirationHandler = {
+            task.setTaskCompleted(success: false)
+            print("Task could not be completed")
+            self.scheduleAppRefresh()
+        }
+        
+        task.expirationHandler = expirationHandler
+        remindersManager.checkForDueReminders(date: Date())
+        
+        print("Task completed")
+        task.setTaskCompleted(success: true)
+        self.scheduleAppRefresh()
     }
 }
