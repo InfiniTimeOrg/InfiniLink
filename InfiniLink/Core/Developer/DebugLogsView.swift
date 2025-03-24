3a483bda8ad9283d40e599865c288c2416564cf8
Liam Willey <chargerelectronics19@gmail.com>
2025-03-24 13:42:54 -0400

    UI: add search function to DebugLogsView and make date more specific

--- InfiniLink/Core/Developer/DebugLogsView.swift
+++ InfiniLink/Core/Developer/DebugLogsView.swift
@@ -12,6 +12,8 @@ struct DebugLogsView: View {
     
     @AppStorage("logSelection") var logSelection = "ble"
     
+    @State private var searchText = ""
+    
     var body: some View {
         TabView(selection: $logSelection) {
             logs(for: .app)
@@ -34,57 +36,71 @@ struct DebugLogsView: View {
     }
     
     func logs(for type: DebugLogTarget) -> some View {
-        let logs = logManager.logs.filter({ $0.target == type })
+        var logs: [DebugLog] {
+            logManager.logs
+                .filter({ $0.target == type })
+                .filter({
+                    if !searchText.isEmpty {
+                        let query = searchText.lowercased()
+                        let body = $0.body.lowercased()
+                        let caller = ($0.caller ?? "").lowercased()
+                        
+                        return body.contains(query) || caller.contains(query)
+                    }
+                    return true
+                })
+        }
         
         return VStack {
+            List(logs.sorted(by: { log1, log2 in
+                return log1.date > log2.date
+            })) { log in
+                VStack(alignment: .leading, spacing: 8) {
+                    Group {
+                        let date = log.date.formatted(.dateTime.day().weekday().month().hour().minute().second())
+                        if let caller = log.caller {
+                            Text(caller + " • " + date)
+                        } else {
+                            Text(date)
+                        }
+                    }
+                    .font(.caption.weight(.semibold))
+                    .foregroundStyle(.gray)
+                    Text(log.body)
+                    if log.type == .error {
+                        let color = (log.type == .error ? Color.red : Color.orange)
+                        
+                        Text(log.type.rawValue.uppercased())
+                            .font(.system(size: 10.5))
+                            .padding(4)
+                            .padding(.horizontal, 4)
+                            .foregroundStyle(color)
+                            .background {
+                                Capsule()
+                                    .stroke(color, lineWidth: 2)
+                            }
+                            .clipShape(Capsule())
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .padding()
+                .contextMenu {
+                    Button {
+                        UIPasteboard.general.string = {
+                            return "\(log.type.rawValue.capitalized) \(log.caller == nil ? "" : "from \(log.caller!)") at \(log.date.formatted()): \(log.body)"
+                        }()
+                    } label: {
+                        Label("Copy", systemImage: "doc.on.clipboard")
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+        }
+        .overlay {
             if logs.isEmpty {
                 Text("No Logs")
                     .foregroundStyle(.gray)
                     .frame(maxHeight: .infinity)
-            } else {
-                List {
-                    ForEach(logs.sorted(by: { log1, log2 in
-                        return log1.date > log2.date
-                    })) { log in
-                        VStack(alignment: .leading, spacing: 8) {
-                            Group {
-                                if let caller = log.caller {
-                                    Text(caller + " • " + log.date.formatted())
-                                } else {
-                                    Text(log.date.formatted())
-                                }
-                            }
-                            .font(.caption.weight(.semibold))
-                            .foregroundStyle(.gray)
-                            Text(log.body)
-                            if log.type == .error {
-                                let color = (log.type == .error ? Color.red : Color.orange)
-                                
-                                Text(log.type.rawValue.uppercased())
-                                    .font(.system(size: 10.5))
-                                    .padding(4)
-                                    .padding(.horizontal, 4)
-                                    .foregroundStyle(color)
-                                    .background {
-                                        Capsule()
-                                            .stroke(color, lineWidth: 2)
-                                    }
-                                    .clipShape(Capsule())
-                            }
-                        }
-                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                        .padding()
-                        .contextMenu {
-                            Button {
-                                UIPasteboard.general.string = {
-                                    return "\(log.type.rawValue.capitalized) \(log.caller == nil ? "" : "from \(log.caller!)") at \(log.date.formatted()): \(log.body)"
-                                }()
-                            } label: {
-                                Label("Copy", systemImage: "doc.on.clipboard")
-                            }
-                        }
-                    }
-                }
             }
         }
     }
@@ -94,9 +110,9 @@ struct DebugLogsView: View {
     NavigationView {
         DebugLogsView()
             .onAppear {
-                #if DEBUG
+#if DEBUG
                 DebugLogManager.shared.logs.append(DebugLog(caller: "DebugLogsView", body: "This is a testing error, designed to be multiple lines long.", type: .error, target: .app, date: Date()))
-                #endif
+#endif
             }
     }
 }
