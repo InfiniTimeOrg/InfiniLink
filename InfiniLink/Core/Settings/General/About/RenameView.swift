90e3f2e32328ef8c15b657fd15372d5533683204
Liam Willey <chargerelectronics19@gmail.com>
2025-05-07 09:32:01 -0400

    Fix app icon issues, add some translations

--- InfiniLink/Core/Settings/General/About/RenameView.swift
+++ InfiniLink/Core/Settings/General/About/RenameView.swift
@@ -19,21 +19,35 @@ struct RenameView: View {
     
     var body: some View {
         List {
-            TextField("InfiniTime", text: $name)
-                .submitLabel(.done)
-                .focused($isFocused)
-                .onSubmit {
-                    dismiss()
-                    
-                    var name = self.name
-                    if name.trimmingCharacters(in: .whitespaces) == "" {
-                        name = "InfiniTime"
+            HStack {
+                TextField("InfiniTime", text: $name)
+                    .submitLabel(.done)
+                    .focused($isFocused)
+                    .onSubmit {
+                        dismiss()
+                        
+                        var name = self.name
+                        if name.trimmingCharacters(in: .whitespaces) == "" {
+                            name = "InfiniTime"
+                        }
+                        
+                        deviceManager.updateName(name: name.trimmingCharacters(in: .whitespaces), for: BLEManager.shared.pairedDevice)
                     }
-                    
-                    deviceManager.updateName(name: name.trimmingCharacters(in: .whitespaces), for: BLEManager.shared.pairedDevice)
+                if isFocused && !name.isEmpty {
+                    Button {
+                        name = ""
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 10).weight(.semibold))
+                            .foregroundStyle(.gray)
+                            .padding(5)
+                            .background(Material.regular)
+                            .clipShape(Circle())
+                    }
                 }
+            }
         }
-        .navigationTitle("Rename")
+        .navigationTitle("Name")
         .onAppear {
             isFocused = true
         }
