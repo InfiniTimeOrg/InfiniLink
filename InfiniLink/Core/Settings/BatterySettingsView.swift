ea540848cb9b3306bdece694d36b1a4db0e1e863
Liam Willey <chargerelectronics19@gmail.com>
2026-04-18 22:18:45 -0400

    Use correct percentage label

--- InfiniLink/Core/Settings/BatterySettingsView.swift
+++ InfiniLink/Core/Settings/BatterySettingsView.swift
@@ -74,10 +74,9 @@ struct BatterySettingsView: View {
                                             Text("0%").font(.caption).foregroundStyle(.secondary)
                                             Slider(value: Binding(
                                                 get: { Double(customBatteryNotificationPercentage) },
-                                                set: { customBatteryNotificationPercentage = Int($0)
-                                                }
+                                                set: { customBatteryNotificationPercentage = Int($0) }
                                             ), in: 0...99, step: 5)
-                                            Text("100%").font(.caption).foregroundStyle(.secondary)
+                                            Text("99%").font(.caption).foregroundStyle(.secondary)
                                         }
                                 }
                             }
